0aafcc1039fb7a20c3957c93e51f4fcfb3137fda
BlankySim <juansimon@live.com.ar>
2020-05-19 11:06:57 -0300

    Project upload

---
+++ ejercicio1/ejer1.cpp
@@ -0,0 +1,15 @@
+#include "ejer1.h"
+
+namespace parcial
+{
+	void mostrarCreditos()
+	{
+		cout << "Blanco Juan Simon" << endl;
+		cout << "Visual Studio 2017" << endl;
+	}
+
+	float diferencia(float num1, float num2)
+	{
+		return num1 - num2;
+	}
+}
